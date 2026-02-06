c71771913f3f3d72b2cb15ebad09d2746fc4215f
Дима Мелков <you@example.com>
2026-02-06 18:40:18 +0300

    2 and 3 задание

---
+++ ru/hse/lab1/Main.java
@@ -0,0 +1,10 @@
+
+import second.Person;
+
+/** * @author - tumasov * @version - 1.0 * @since - 2026 */public class Main {
+    public static void main(String[] args) {
+        Person Semen = new Person("Semen", 18);
+        System.out.println("Имя: " + Semen.name);
+        System.out.println("Возраст: " + Semen.age);
+    }
+}
